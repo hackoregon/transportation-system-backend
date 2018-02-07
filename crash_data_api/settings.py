ae0c442a0f6487c44b7d5e4da185f760b91d9e26
kiniadit <aditya.kini.m@gmail.com>
2018-02-07 10:26:59 -0800

    added env.sample

--- crash_data_api/settings.py
+++ crash_data_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'django_filters',
+    'rest_framework_swagger',
     ]
 
 MIDDLEWARE = [
@@ -122,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = '.'
 
 STATIC_URL = '/static/'
 
