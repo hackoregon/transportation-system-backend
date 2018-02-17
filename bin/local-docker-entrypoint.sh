a798e9094d8dfc4f59508432ccc880619f2d439a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-16 21:24:53 -0800

    wait for "postgres", not the user database (issue #35)

--- bin/local-docker-entrypoint.sh
+++ bin/local-docker-entrypoint.sh
@@ -6,7 +6,7 @@
 set -e
 
 export PGPASSWORD=$POSTGRES_PASSWORD
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d "$POSTGRES_NAME" -c '\q'
+until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
 do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 15
