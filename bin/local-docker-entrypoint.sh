57fcb428d3d1b55e2d2e60a8310da6582a21a663
kiniadit <aditya.kini.m@gmail.com>
2018-02-17 16:08:13 -0800

    fixed migration issue

--- bin/local-docker-entrypoint.sh
+++ bin/local-docker-entrypoint.sh
@@ -3,16 +3,26 @@
 # wait-for-postgres.sh
 # https://docs.docker.com/compose/startup-order/
 
-set -e
+# set -e
 
-export PGPASSWORD=$POSTGRES_PASSWORD
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
-do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 15
-done
+# export PGPASSWORD=$POSTGRES_PASSWORD
+# until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
+# do
+#   >&2 echo "Postgres is unavailable - sleeping"
+#   sleep 15
+# done
 
->&2 echo "Postgres is up"
+# >&2 echo "Postgres is up"
 
-python manage.py migrate
+# Collect static files
+echo "Collect static files"
+./manage.py collectstatic --noinput
+
+echo "Make migrations"
+./manage.py makemigrations
+
+echo "Migrate"
+./manage.py migrate
+
+echo "Run server..."
 ./manage.py runserver 0.0.0.0:8000
