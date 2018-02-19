fe5c9318b95561a2ef883c6a5a5ee9521d66202c
Brian Grant <brian.grant@urbanairship.com>
2018-02-19 14:35:31 -0800

    add try catch for monkeypatch

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -66,7 +66,29 @@ backlog = 2048
 #
 
 workers = 4
-worker_class = 'gevent'
+
+try:
+    # fail 'successfully' if either of these modules aren't installed
+    from gevent import monkey
+    from psycogreen.gevent import patch_psycopg
+
+
+    # setting this inside the 'try' ensures that we only
+    # activate the gevent worker pool if we have gevent installed
+    worker_class = 'gevent'
+
+    # this ensures forked processes are patched with gevent/gevent-psycopg2
+    def do_post_fork(server, worker):
+        monkey.patch_all()
+        patch_psycopg()
+
+        # you should see this text in your gunicorn logs if it was successful
+        worker.log.info("Made Psycopg2 Green")
+
+    post_fork = do_post_fork
+except ImportError:
+    pass
+
 # worker_connections = 1000
 # timeout = 30
 # keepalive = 2
@@ -177,10 +199,6 @@ proc_name = None
 #       A callable that takes a server instance as the sole argument.
 #
 
-# Enable greenlet support in psycopg
-def post_fork(server, worker):
-    from psycogreen.gevent import patch_psycopg
-    patch_psycopg()
 
 def pre_fork(server, worker):
     pass
